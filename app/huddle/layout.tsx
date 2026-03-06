9c847b242b70915b56d28deef5b1497a4b064f62
sagar-johari <joharisagar53@gmail.com>
2026-03-06 10:53:45 +0530

    changes in layout of huddle

--- app/huddle/layout.tsx
+++ app/huddle/layout.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return <Suspense fallback={null}>{children}</Suspense>;
 }

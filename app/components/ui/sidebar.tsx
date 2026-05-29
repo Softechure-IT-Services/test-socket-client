8b2f24045f3baa96e893efee0161a6f6afb666db
sagar-johari <joharisagar53@gmail.com>
2026-05-29 12:51:32 +0530

    huddle bottom controls hiding mobile

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -460,7 +460,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
       data-slot="sidebar-inset"
       className={cn(
         "bg-background z-[9] relative flex w-full flex-1 flex-col overflow-auto",
-        "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2 h-[calc(100vh-40px)] sm:h-[calc(100vh-56px)] mt-0 pt-[40px] ",
+        "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2 h-[calc(100vh-80px)] sm:h-[calc(100vh-56px)] mt-0 pt-[40px] ",
         className
       )}
       {...props}
